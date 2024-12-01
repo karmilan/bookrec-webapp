e658b0fe82ba50e76820574a59c83345440aa3d8
karmilan <delftkarmilan@gmail.com>
2024-12-01 12:51:07 +0530

    register link updated

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -30,7 +30,11 @@ const RegisterForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5000/api/auth/register", formData);
+      // await axios.post("http://localhost:5000/api/auth/register", formData);
+      await axios.post(
+        `${import.meta.env.VITE_API_URL}/auth/register`,
+        formData
+      );
       navigate("/login");
     } catch (error) {
       console.log(error);
